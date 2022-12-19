07d14ccb00ca33d5cb7c18dda9d987c1e7822b16
Joshua Cooper <65969149+joshcooper6@users.noreply.github.com>
2022-12-19 14:55:50 -0800

    style changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled, { keyframes } from 'styled-components';
+
+const FadeIn = keyframes`
+  from { opacity: 0 } to { opacity: 1 }
+`
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 100vw;
+  min-height: 100vh;
+  background: url(${props => props.bg});
+  background-size: cover;
+  background-position: center;
+  filter: blur(1em);
+  -webkit-filter: blur(1em);
+  z-index: -1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  scale: 1.1;
+`
+
+const ButtonImg = styled.div`
+    background: url(${props => props.imgSrc});
+    background-size: cover;
+    background-position: center;
+    height: 80px;
+    width: 80px;
+    border-radius: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 1em;
+    // opacity: ${props => props.showProceed ? 0.5 : 1};
+    z-index: 0;
+`
+
+const Button = styled.button`
+  display: ${props => props.showProceed ? 'block' : 'none'};  
+  height: 100%;
+  width: 100%;
+  border: 0;
+  border-radius: 100%;
+  opacity: ${props => props.showProceed ? '1' : '0'};
+  background-color: rgba(0, 0, 0, 0.9);
+  color: white;
+  cursor: pointer;
+  background-blend-mode: multiply;
+  font-weight: 800;
+  transition: 1s ease;
+  font-size: 2em;
+  animation: ${FadeIn} 500ms linear forwards;
+`
+
+
 
 const QuizApp = () => {
   const [questions, setQuestions] = useState([]);
@@ -9,7 +68,6 @@ const QuizApp = () => {
   const [isLoading, setIsLoading] = useState(true);
   const totalQuestions = questions.length;
   const correctAnswers = results.filter(results => results.result === true).length;
-
   const [currentChoice, setCurrentChoice] = useState('');
   const [showProceed, setShowProceed] = useState(false);
 
@@ -19,18 +77,6 @@ const QuizApp = () => {
       setIsLoading(false);
     });
   }, []);
-
-  // useEffect(() => {
-  //   setShowProceed(true)
-  // }, [currentChoice]);
-
-  // useEffect(() => {
-  //   console.log(totalQuestions);
-  // }, [questions]);
-
-  // useEffect(() => {
-  //   console.log(answers);
-  // }, [answers]);
 
   const fanType = (score) => {
     if (score <= 0) { return `You have no idea who she is.` }
@@ -55,7 +101,7 @@ const QuizApp = () => {
       questionId: questions[currentQuestionIndex].id,
       answer
     }).then(res => {
-      setCurrentQuestionIndex(prevIndex => prevIndex + 1);
+      nextQuestion();
       setResults([...results, {
         id: questions[currentQuestionIndex].id,
         result: res.data.correct
@@ -74,17 +120,29 @@ const QuizApp = () => {
 
   const currentQuestion = questions[currentQuestionIndex];
 
+  // window.onblur = function() {
+  //   setLeft(true)
+  // };
+
+
   return (
+    <>
+    <Body bg={currentQuestion.image} />
     <div className="question-wrap">
-      <p>{currentQuestion.id}. {currentQuestion.text}</p>
+      <div className="question-header">
+        <p className="question">{currentQuestion.text}</p>
+        <ButtonImg showProceed={showProceed} imgSrc={currentQuestion.image}>
+          <Button showProceed={showProceed} children={'>'} onClick={() => handleAnswerClick(currentChoice)} />
+        </ButtonImg>
+      </div>
       {currentQuestion.choices.map(choice => (
-        <button className={currentChoice === choice && 'selected'} key={choice} onClick={() => {setCurrentChoice(choice); setShowProceed(true)} }>{choice}</button>
+        <button id={currentChoice === choice && 'selected'} key={choice} onClick={() => {setCurrentChoice(choice); setShowProceed(true)} }>{choice}</button>
       ))}
 
-      { showProceed ? <button id={'proceed-btn'} children={'>>'} onClick={() => handleAnswerClick(currentChoice)} /> : null }
+      {/* { showProceed ? <button children={'>>'} onClick={() => handleAnswerClick(currentChoice)} /> : null } */}
 
-      <img src={currentQuestion.image} className="ques_img" />
     </div>
+    </>
   );
 };
 
